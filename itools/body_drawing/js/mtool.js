b03fed69ee5174e60d69c42c860e589491d11891
David Mah <MahHaha@gmail.com>
2013-01-04 15:21:09 -0800

    Drawing and JSON Output

--- itools/body_drawing/js/mtool.js
+++ itools/body_drawing/js/mtool.js
@@ -9,6 +9,7 @@ head.js("js/lib/jquery-1.8.3.min.js");
 
 head.ready(function() {
 
+  // Draws a circle to the container. returns the radius
   function drawCircle(container, x, y, x2, y2, color, fill) {
     var radius = Math.sqrt(Math.pow(x - x2, 2) + Math.pow(y - y2, 2));
     var shape = new createjs.Shape();
@@ -20,23 +21,96 @@ head.ready(function() {
       shape.graphics.beginStroke(color);
     shape.graphics.drawCircle(0, 0, radius);
     container.addChild(shape);
+    return radius;
   }
+
+  function setBackgroundImage(image_uri) {
+    background.removeAllChildren();
+    var image = new createjs.Bitmap(image_uri);
+    background.addChild(image);
+  }
+
+  function outputBody(states) {
+    outputArea = jQuery("#output");
+    outputArea.text(outputArea.text() + "\n" + JSON.stringify(createNewBody(states)) + "\n");
+  }
+
+  // Creates a body from default using the given states
+  function createNewBody(states) {
+    return {
+              health: 3,
+              x: 0,
+              y: 0,
+              entryTime: 0,
+              states: states
+           };
+  }
+
+  // Creates a State from default using the given subbodies and image
+  function createNewState(subbodies, image) {
+    return {
+              subbodies: subbodies,
+              time: 5,
+              image: image,
+              attackSequence: null
+           };
+  }
+
+  function createNewSubbody(x, y, r) {
+    return {
+              x: x,
+              y: y,
+              radius: r
+           };
+  }
+
+  var currentStates = [];
+  function resetStates() {
+    currentStates = [];
+  }
+
+  function addState(subbodies, image) {
+    currentStates.push(createNewState(subbodies, image));
+    console.log(currentStates);
+  }
+
+  var currentSubbodies = [];
+
+  function resetSubbodies() {
+    currentSubbodies = [];
+  }
+
+  function addSubbody(x, y, r) {
+    currentSubbodies.push(createNewSubbody(x, y, r));
+    console.log(currentSubbodies);
+  }
+
+
+
+
 
   var stage      = new createjs.Stage("gameCanvas");
   // Circles that are part of the finished product
   var finished   = new createjs.Container();
   // Circles part of the temporary mouse held down drawing
   var incomplete = new createjs.Container();
+  var background = new createjs.Container();
 
+  var currentBackgroundImage = "assets/images/zone1.jpg";
+  setBackgroundImage(currentBackgroundImage);
+
+  stage.addChild(background);
   stage.addChild(finished);
   stage.addChild(incomplete);
 
   var start_x;
   var start_y;
 
+
   // Does the user current have the mouse pressed to draw?
   var drawing = false;
 
+  // User Controls
   stage.onMouseDown = function(event) {
     start_x = event.stageX;
     start_y = event.stageY;
@@ -48,7 +122,9 @@ head.ready(function() {
   stage.onMouseUp = function(event) { 
     incomplete.removeAllChildren();
 
-    drawCircle(finished, start_x, start_y, event.stageX, event.stageY, 'blue', true);
+    var radius = drawCircle(finished, start_x, start_y, event.stageX, event.stageY, 'blue', true);
+    addSubbody(start_x, start_y, radius);
+
     drawing = false;
   };
 
@@ -60,6 +136,24 @@ head.ready(function() {
       drawCircle(incomplete, start_x, start_y, event.stageX, event.stageY, 'green', false);
     }
   };
+
+  // On key press
+  jQuery(window).keyup(function(event) {
+    if (event.keyCode == 32 || event.keyCode == 13) {
+      addState(currentSubbodies, currentBackgroundImage);
+      resetSubbodies();
+    }
+    if (event.keyCode == 13) {
+      outputBody(currentStates);
+      resetStates();
+    }
+  });
+
+
+
+
+
+
 
   // Visual Frame updates should happen at this time
   setInterval(function() {
